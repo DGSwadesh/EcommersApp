30707cf6c493a6ff535329a5c06d50d472494bd9
Dibyendu <ghoshdibyendu254@gmail.com>
2021-07-30 16:28:26 +0530

    dg

--- lib/View/Prasanta/SigninSignUp/loginPage.dart
+++ lib/View/Prasanta/SigninSignUp/loginPage.dart
@@ -1,3 +1,5 @@
+//import 'dart:js';
+import 'package:e_commerce_app/View/Amit/DashBoard/Dashboard.dart';
 import 'package:e_commerce_app/util/constant.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
@@ -9,6 +11,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  ScrollController controller = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +22,12 @@ class _LoginPageState extends State<LoginPage> {
         width: 1.sw,
         color: red,
         child: SingleChildScrollView(
+          controller: controller,
           child: Stack(
             children: [
               Column(
                 children: [
-                  loginContainer(),
+                  loginContainer(context),
                   signUpContainer(),
                 ],
               ),
@@ -33,14 +38,13 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-}
 
-loginContainer() {
+loginContainer(context) {
   return Stack(
     children: [
       Container(
         child: CustomPaint(
-          size: Size(1.sw, 0.8.sh),
+          size: Size(1.sw, 0.9.sh),
           painter: RPSCustomPainter(),
         ),
       ),
@@ -65,16 +69,16 @@ loginContainer() {
                 autovalidateMode: AutovalidateMode.always,
                 decoration: const InputDecoration(
                   icon: Icon(Icons.person),
-                  hintText: 'What do people call you?',
-                  labelText: 'Email *',
+                  hintText: 'Enter Your Email',
+                  labelText: 'Email',labelStyle: TextStyle(fontWeight:FontWeight.bold)
                 ),
               ),
               TextFormField(
                 autovalidateMode: AutovalidateMode.always,
                 decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: 'What do people call you?',
-                  labelText: 'Email *',
+                   icon: Icon(Icons.vpn_key),
+                  hintText: 'Enter Your Password',
+                  labelText: 'Password',labelStyle: TextStyle(fontWeight:FontWeight.bold,)
                 ),
               ),
               Material(
@@ -88,31 +92,55 @@ loginContainer() {
                       child: Text(
                         'Forget Password',
                         style: TextStyle(
-                            fontSize: 12,
+                            fontSize: 14,
                             color: black,
                             height: 2,
-                            fontWeight: FontWeight.w300),
+                            fontWeight: FontWeight.bold),
                       ),
                     ),
                   ),
                 ),
               ),
+              SizedBox(height: 20),
               Padding(
-                padding: EdgeInsets.only(top: 40, left: 0.7.sw),
-                child: Container(
-                  width: 100,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: red, borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.only(top: 40, left: 0.4.sw),
+                child: InkWell(
+                  child: Container(
+                    width: 200,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [Colors.orangeAccent, Colors.pinkAccent]),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Center(
+                        child: Text(
+                      "Login",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    )),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => DashBoard()));
+                  },
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(top: 70, left: 0.8.sw),
-                child: Container(
-                  width: 30,
-                  height: 30,
-                  decoration: BoxDecoration(
-                      color: red, borderRadius: BorderRadius.circular(100)),
+                padding: EdgeInsets.only(top:80, left: 0.8.sw),
+                child: InkWell(
+                  onTap: (){
+                    controller.jumpTo(450);
+                  },
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.pinkAccent,
+                        borderRadius: BorderRadius.circular(100)),
+                    child: Icon(Icons.arrow_downward_sharp, color: white),
+                  ),
                 ),
               ),
             ],
@@ -124,112 +152,133 @@ loginContainer() {
 }
 
 signUpContainer() {
-  return Stack(
-    children: [
-      RotatedBox(
-        quarterTurns: 2,
-        child: Container(
-          child: CustomPaint(
-            size: Size(1.sw, 0.8.sh),
-            painter: RPSCustomPainter(),
-          ),
+  return Stack(children: [
+    RotatedBox(
+      quarterTurns: 2,
+      child: Container(
+        child: CustomPaint(
+          size: Size(1.0.sw, 1.0.sh),
+          painter: RPSCustomPainter(),
         ),
       ),
-      Padding(
+    ),
+    Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
-          height: 0.8.sh,
-          width: 1.sw,
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 7, right: 0.8.sw),
-                child: Container(
-                  width: 30,
-                  height: 30,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(100)),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 7, right: 0.7.sw),
-                child: Text(
-                  'LOGO',
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: black,
-                      height: 2,
-                      fontWeight: FontWeight.w500),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  top: 50,
-                ),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      autovalidateMode: AutovalidateMode.always,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.person),
-                        hintText: 'What do people call you?',
-                        labelText: 'Email *',
-                      ),
-                    ),
-                    TextFormField(
-                      autovalidateMode: AutovalidateMode.always,
-                      decoration: const InputDecoration(
-                        icon: Icon(Icons.person),
-                        hintText: 'What do people call you?',
-                        labelText: 'Email *',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: () async {},
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 50),
-                    child: Align(
-                      alignment: Alignment.topLeft,
-                      child: Text(
-                        'Forget Password',
-                        style: TextStyle(
-                            fontSize: 12,
-                            color: black,
-                            height: 2,
-                            fontWeight: FontWeight.w300),
-                      ),
-                    ),
+            height: 0.8.sh,
+            width: 1.sw,
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top:20.0, right: 0.8.sw),
+                  child:  InkWell(
+                  onTap: (){
+                    controller.jumpTo(0);
+                  },
+                    child: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(100)),
+                        child:
+                            Icon(Icons.arrow_upward_sharp, color: Colors.white)),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 40, left: 0.7.sw),
-                child: Container(
-                  width: 100,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: red, borderRadius: BorderRadius.circular(10)),
+                SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.only(top: 7, right: 0.7.sw),
+                  child: Text(
+                    'LOGO',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: black,
+                        height: 2,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 50,
+                  ),
+                  child: SingleChildScrollView(
+                      child: Column(
+                    children: [
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.person),
+                          hintText: 'What do people call you?',
+                          labelText: 'Name',labelStyle: TextStyle(fontWeight:FontWeight.bold)
+                ),
+                        ),
+                      
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.email),
+                          hintText: 'What do people call you?',
+                          labelText: 'Email',labelStyle: TextStyle(fontWeight:FontWeight.bold)
+                ),
+                        ),
+                    
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.vpn_key),
+                          hintText: 'What do people call you?',
+                          labelText: 'Password',labelStyle: TextStyle(fontWeight:FontWeight.bold)
+                ),
+                        ),
+                      
+                      TextFormField(
+                        autovalidateMode: AutovalidateMode.always,
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.vpn_key),
+                          hintText: 'What do people call you?',
+                          labelText: 'Comfirm Password',labelStyle: TextStyle(fontWeight:FontWeight.bold)
+                ),
+                        ),
+        
+                    ],
+                  )),
+                ),
+                SizedBox(height:20),
+                 Padding(
+                padding: EdgeInsets.only(top:40, right: 0.4.sw),
+                child: InkWell(
+                  child: Container(
+                    width: 200,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [Colors.orangeAccent, Colors.pinkAccent]),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Center(
+                        child: Text(
+                      "Sign Up",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    )),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => DashBoard()));
+                  },
                 ),
               ),
-            ],
-          ),
-        ),
-      )
-    ],
-  );
+              ],
+            )))
+  ]);
 }
 
 middleButtonContainer() {
   return Padding(
-    padding: EdgeInsets.only(top: 0.5.sh),
+    padding: EdgeInsets.only(top: 0.6.sh),
     child: Container(
       width: 1.sw,
       height: 0.5.sh,
@@ -237,30 +286,44 @@ middleButtonContainer() {
       child: Column(
         children: [
           Padding(
-            padding: EdgeInsets.only(top: 0.05.sh, right: 0.8.sw),
+            padding: EdgeInsets.only(top: 0.12.sh, right: 0.7.sw),
             child: Container(
-              width: 30,
-              height: 30,
+              width: 50,
+              height: 50,
               decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(100)),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(100)),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(70),
+                  child: Image.asset("Images/G+.jpg", fit: BoxFit.fill)),
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 0.08.sh, right: 0.5.sw),
+            padding: EdgeInsets.only(top: 0.04.sh, right: 0.01.sw),
             child: Container(
-              width: 30,
-              height: 30,
+              width: 50,
+              height: 50,
               decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(100)),
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(100)),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(70),
+                  child: Image.asset("Images/fb1.jpg", fit: BoxFit.fill)),
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(top: 0.1.sh, right: 0.1.sw),
-            child: Container(
-              width: 30,
-              height: 30,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(100)),
+            padding: EdgeInsets.only(top: 0.05.sh, left: 0.7.sw),
+            child: Flexible(
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(100)),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(70),
+                    child: Image.asset("Images/twitter1.jpg", fit: BoxFit.fill)),
+              ),
             ),
           ),
         ],
@@ -268,7 +331,7 @@ middleButtonContainer() {
     ),
   );
 }
-
+}
 class RPSCustomPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
